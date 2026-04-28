30ad0c5172d2bb7ba0b04f29401bf7a441c800f0
narendraAnalytics <narendra.insights@gmail.com>
2026-04-28 21:38:34 +0530

    Added added serverExternalPackages: ['@google/adk', '@google/genai']

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: ['@google/adk', '@google/genai'],
 };
 
 export default nextConfig;
